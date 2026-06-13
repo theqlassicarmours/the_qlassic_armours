70a6abdbcf653239c22b38a34da0a0237d547a2c
Mousham <88428216+moushamdev@users.noreply.github.com>
2026-06-13 14:07:55 +0530

    Update footer section content and links

--- src/sections/FooterSection.jsx
+++ src/sections/FooterSection.jsx
@@ -50,24 +50,24 @@ const FooterSection = () => {
         <div className="mt-40 md:px-10 px-5 flex gap-10 md:flex-row flex-col justify-between text-milk font-paragraph md:text-lg font-medium">
           <div className="flex items-center md:gap-16 gap-5">
             <div>
-              <p>SPYLT Flavors</p>
+              <p>Qlassic Armor</p>
             </div>
             <div>
-              <p>Chug Club</p>
-              <p>Student Marketing</p>
-              <p>Dairy Dealers</p>
+  <p>Style Club</p>
+<p>Custom Orders</p>
+<p>Partner Creators</p>
             </div>
             <div>
-              <p>Company</p>
-              <p>Contacts</p>
-              <p>Tasty Talk</p>
+<p>Company</p>
+<p>Contact Us</p>
+<p>Style Journal</p>
             </div>
           </div>
 
 <div className="md:max-w-lg">
   <p>
-    Get Exclusive Early Access and Stay Informed About Product
-    Updates, Events, and More!
+Get Exclusive Early Access and Stay Updated About New
+Collections, Offers, and More!
   </p>
 
   <div className="flex justify-between items-center border-b border-[#80c4d6] py-5 md:mt-10">
@@ -88,7 +88,7 @@ const FooterSection = () => {
 
 <div className="copyright-box">
   {/* The final row with copyright and legal links. */}
-  <p>Copyright © 2025 Spylt - All Rights Reserved</p>
+<p>Copyright © 2025 Qlassic Armor - All Rights Reserved</p>
   <div className="flex items-center gap-7">
     <p>Privacy Policy</p>
     <p>Terms of Sеrvice</p>
