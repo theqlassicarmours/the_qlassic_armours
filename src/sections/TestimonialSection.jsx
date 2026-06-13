ac66d84a0d3564ecccca68131812f4a79e14a1f6
Mousham <88428216+moushamdev@users.noreply.github.com>
2026-06-13 14:12:33 +0530

    Update testimonial section titles

--- src/sections/TestimonialSection.jsx
+++ src/sections/TestimonialSection.jsx
@@ -68,9 +68,9 @@ const TestimonialSection = () => {
   return (
     <section className="testimonials-section">
       <div className="absolute size-full flex flex-col items-center pt-[5vw]">
-        <h1 className="text-black first-title">What's</h1>
-        <h1 className="text-light-brown sec-title">Everyone</h1>
-        <h1 className="text-black third-title">Talking</h1>
+<h1 className="text-black first-title">What's</h1>
+<h1 className="text-light-brown sec-title">Everyone</h1>
+<h1 className="text-black third-title">Wearing</h1>
       </div>
 
       <div className="pin-box">
